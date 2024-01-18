75847ea77f8f2a421f11d254837bc7955ec0e0b2
yonaides@hotmail.com <yonaides@Yonaidess-MacBook-Air.local>
2024-01-18 15:04:18 -0400

    Realización de cambios

--- src/main/java/com/jwt/seguridad/spring/springbootjwt/mapper/MapperConfig.java
+++ src/main/java/com/jwt/seguridad/spring/springbootjwt/mapper/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.jwt.seguridad.spring.springbootjwt.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,8 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
+        ModelMapper moderMapper = new ModelMapper();
+        moderMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         return new ModelMapper();
     }
 }
